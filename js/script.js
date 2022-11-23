1b4d3f89288d5436e638ffa157930c66a85a8ef9
vinithbalu <rbvinithraja@gmail.com>
2022-11-23 16:03:33 +1100

    updated

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changeText()
+{
+    var textsArray = ["TEXT1","TEXT2", "TEXT3", "TEXT4", "TEXT5"]
+    var number = getRandomNumberBetween(0,textsArray.length - 1)
+    console.log("Index: ", number)
+    document.getElementById("heading").innerHTML = textsArray[number];
+}
+function getRandomNumberBetween(min,max)
+{
+    return Math.floor(Math.random()*(max-min+2)+min);
+}
